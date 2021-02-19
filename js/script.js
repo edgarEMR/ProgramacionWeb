aa71a253f53126a212e798e763432c9b046830eb
Edgar <edgarmreyna@outlook.com>
2021-02-18 21:34:43 -0600

    Clase 3 - Formularios y Javascript

---
+++ js/script.js
@@ -0,0 +1,55 @@
+/*const PI = 3.1416;
+
+console.log(PI);
+
+const car = {tipo: "Toyota", modelo: "2018", color: "negro"};
+
+car.color = "blanco";
+
+car.dueno = "Edgar";
+
+console.log(car);
+*/
+
+let i = 5;
+for(let i = 0; i<10; i++){
+
+}
+
+console.log(i);
+/*
+var y;
+function ejemplo(){
+    y = 100 * 20;
+    var z = 10;
+    console.log("z: " + z);
+}
+
+ejemplo();
+
+y = 1;
+
+console.log("y: " + y);
+
+var x = 0;
+
+function a(){
+    var y = 2;
+
+    console.log(x,y);
+
+    function b(){
+        x = 3;
+        y = 4;
+        z = 5;
+    }
+
+    b();
+
+    console.log(x,y,z);
+}
+
+a();
+
+console.log(x,z);
+*/
